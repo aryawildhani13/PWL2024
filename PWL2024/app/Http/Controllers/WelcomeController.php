20b4fd2c0e189745b610976ae544f473cd5a8d13
aryawildhani13 <aryawildhaniputra@gmail.com>
2024-02-24 18:56:02 +0700

    update

--- PWL2024/app/Http/Controllers/WelcomeController.php
+++ PWL2024/app/Http/Controllers/WelcomeController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use COM;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
     public function hello(){
-        return 'hello World via controller';
+        return 'hello World';
+    }
+    public function greeting(){
+        return view('blog.hello')
+            ->with('name','Andi')
+            ->with('occupation','Astronaut');
     }
 }
+
