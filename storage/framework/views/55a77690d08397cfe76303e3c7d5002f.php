0b770a2653fa95a19d540f8a9702f659050122cc
aryawildhani13 <aryawildhaniputra@gmail.com>
2024-04-15 17:46:56 +0700

    -

--- storage/framework/views/55a77690d08397cfe76303e3c7d5002f.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-        <b>Version</b> 3.2.0
-    </div>
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights
-    reserved.
-</footer>
-<?php /**PATH D:\laragon\www\PWL2024\PWL_POS_Upgrade\resources\views/layouts/footer.blade.php ENDPATH**/ ?>
